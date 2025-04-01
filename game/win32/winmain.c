8b0baff0981c59c8bdddebdcd0d3dbf4c7d82d05
KesslerCascade <117657415+KesslerCascade@users.noreply.github.com>
2025-04-01 14:40:19 -0500

    Remove WinMain debug popup

--- game/win32/winmain.c
+++ game/win32/winmain.c
@@ -5,11 +5,6 @@
 // Wrap WinMain to call it with the correct module handle
 int subspace_WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-    MessageBoxA(NULL,
-                "Hey it's not the real WinMain",
-                "WinMain (or is it???)",
-                MB_ICONQUESTION | MB_OKCANCEL);
-
     HINSTANCE SubspaceModule = GetModuleHandleA(NULL);
     // Call FTL's WinMain but using our module handle instead of the base address of the loaded
     // executable. We copied resources and TLS data already so it will be able to find them in the
