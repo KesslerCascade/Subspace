0ff56e6f76db58a8b4c5f7e15d28274c3bc4d05d
KesslerCascade <117657415+KesslerCascade@users.noreply.github.com>
2025-06-06 02:12:40 -0500

    Don't use balloon tips
    They don't seem to work on release builds anyway -- probably common
    controls version in them manifest -- but regular tips just look better.

--- subspace/ui/util/settip.c
+++ subspace/ui/util/settip.c
@@ -6,10 +6,5 @@ void setTip(Ihandle* ih, strref tip, strref title, int icon)
     bool useballoon = !osIsWine();   // balloon tooltips don't work well on wine
 
     IupSetAttribute(ih, "TIP", strC(tip));
-    if (!strEmpty(title)) {
-        IupSetAttribute(ih, "TIPBALLOON", useballoon ? "YES" : "NO");
-        IupSetAttribute(ih, "TIPBALLOONTITLE", strC(title));
-        IupSetInt(ih, "TIPBALLOONTITLEICON", icon);
-    }
     IupSetInt(ih, "TIPDELAY", 30000);
 }
