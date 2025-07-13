bd1509e2d252114d8acb98fbc1d00280a2c17956
KesslerCascade <117657415+KesslerCascade@users.noreply.github.com>
2025-07-13 14:22:39 -0500

    Show version 'Dev Snapshot' in non-release builds

--- game/version.c
+++ game/version.c
@@ -3,5 +3,9 @@
 const int subspace_version_maj      = SUBSPACE_MAJOR;
 const int subspace_version_min      = SUBSPACE_MINOR;
 const char* subspace_version_series = SUBSPACE_SERIES;
+#ifdef SUBSPACE_RELEASE
 const char* subspace_version_str    = SUBSPACE_MAJOR_STRING
     "." SUBSPACE_MINOR_STRING "-" SUBSPACE_SERIES;
+#else
+const char* subspace_version_str = "Dev Snapshot";
+#endif
