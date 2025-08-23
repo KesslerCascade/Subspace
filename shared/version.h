d46d3e1c7c283291da9ecf7d6ee0205e6111600f
KesslerCascade <117657415+KesslerCascade@users.noreply.github.com>
2025-08-23 00:00:54 -0500

    Version 0.15 alpha

--- shared/version.h
+++ shared/version.h
@@ -5,11 +5,11 @@
 
 #define SUBSPACE_MAJOR        0
 #define SUBSPACE_MAJOR_STRING VERSION_TO_STR(SUBSPACE_MAJOR)
-#define SUBSPACE_MINOR        14
+#define SUBSPACE_MINOR        15
 #define SUBSPACE_MINOR_STRING VERSION_TO_STR(SUBSPACE_MINOR)
 #define SUBSPACE_SERIES       "alpha"
 
-#define SUBSPACE_WINVER "0.0.14.0"
+#define SUBSPACE_WINVER "0.0.15.0"
 
 extern const int subspace_version_maj;
 extern const int subspace_version_min;
